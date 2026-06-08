f0eeabd8e91835dbc932fb7927ccebf1440f9ccc
Fleur Caval <fcaval@c1r1p4.42lehavre.fr>
2026-06-08 16:19:52 +0200

    structure chunker

--- src/chunker.py
+++ src/chunker.py
@@ -6,7 +6,7 @@
 #  By: fcaval <fcaval@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/06/08 14:35:44 by fcaval          #+#    #+#               #
-#  Updated: 2026/06/08 15:25:01 by fcaval          ###   ########.fr        #
+#  Updated: 2026/06/08 16:18:20 by fcaval          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -18,7 +18,14 @@ from typing import List, Tuple
 Chunk = Tuple[str, int, int, str]
 
 
+#  Découpe texte en chunks en utilisant découpage par sections (car Markdown)
+def chunk_text(file_path: str, content: str, max_size: int) -> List[Chunk]:
+    faire le code section et après le python 
+
+
 #  Entrée -> choisit stratégie chunking
 def chunk_choice(file_path: str, content: str, max_size: int) -> List[Chunk]:
     if file_path.find(".py"):
         return chunk_python(file_path, content, max_size)
+    else:
+        return chunk_text(file_path, content, max_size)
