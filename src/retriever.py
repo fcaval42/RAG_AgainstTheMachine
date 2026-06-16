0e7731b484a2d8a44de059cbe9bd20791615077e
Fleur Caval <fcaval@c1r1p4.42lehavre.fr>
2026-06-16 12:01:53 +0200

    retriever ok

--- src/retriever.py
+++ src/retriever.py
@@ -6,7 +6,7 @@
 #  By: fcaval <fcaval@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/06/15 15:36:23 by fcaval          #+#    #+#               #
-#  Updated: 2026/06/15 16:16:15 by fcaval          ###   ########.fr        #
+#  Updated: 2026/06/16 11:03:26 by fcaval          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -17,11 +17,11 @@ from src.structure import MinimalSource
 
 #  cherche les k chunks, pour une seule question, donc on refait la même pour
 #  request que ce qu'on a fait pour indexer
-def search(request: str, retriever: bm25s.BM25, chunk_metadata:
+def search(request: List[str], retriever: bm25s.BM25, chunk_metadata:
            List[Tuple[str, int, int]], k: int = 5) -> List[MinimalSource]:
 
     # mettre faux show parce que sinon on va avoir une barre de progression
-    tokenized_request = bm25s.tokenize([request], stopwords="en",
+    tokenized_request = bm25s.tokenize(request, stopwords="en",
                                        show_progress=False)
 
     # éviter dépassement
@@ -31,6 +31,14 @@ def search(request: str, retriever: bm25s.BM25, chunk_metadata:
     results, scores = retriever.retrieve(tokenized_request, k=mini_k,
                                          show_progress=False)
 
-    
+    all_sources = []
+    for request_result in results:
+       sources = []
+       for idx in request_result:
+            file_path, start, end = chunk_metadata[int(idx)]
+            sources.append(MinimalSource(file_path=file_path,
+                                         first_character_index=start,
+                                         last_character_index=end))
+       all_sources.append(sources)
 
-#  recherche pour plusieurs questions d'un coup (plus efficace)
+       return all_sources
